b4687480cb7a86f01f30a56ddf65bc81303c541a
Pintu Rathod <60134479+rathodpintu@users.noreply.github.com>
2026-06-10 12:59:16 +0530

    Move Zero
    Move Zero

---
+++ MoveZero.js
@@ -0,0 +1,16 @@
+
+const arr =[1,3,0,7,0,5,66,0,6,8,9]
+function moveZero(arr){
+    let count =0
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] !==0){
+            let temp = arr[i]
+            arr[i]= arr[count]
+            arr[count]= temp
+            count++
+        }
+    }
+    return arr
+}
+
+console.log(moveZero(arr));
